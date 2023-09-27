60569f0c8a8c29d2599ca7ff689589aa423dc99d
Devarsh <devarshsoni001@gmail.com>
2023-09-27 22:23:24 +0530

    Initial commit

---
+++ class/03external.js
@@ -0,0 +1,12 @@
+/*
+ // document.write("BODY")
+document.getElementById('xyz').innerHTML = "DIV"
+*/
+
+window.alert("window alert..!");
+window.confirm("Are you sure?");
+window.prompt("5 + 5 = ? ");
+window.print();
+
+document.getElementById('devarsh').innerHTML = "Devarsh Soni"
+document.getElementById('devarsh').innerText = "Devarsh H. Soni"
